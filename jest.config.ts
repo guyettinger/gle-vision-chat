16f805d041d98c39e807f0714c22b8ad497e220c
guyettinger <guy@flaire.co>
2025-09-29 13:28:45 -0400

    unit tests

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '.',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
+  },
+  setupFiles: ['<rootDir>/jest.setup.js'],
+};
+
+export default config;
---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+process.env.NEXT_PUBLIC_WEBSITE_URL =
+  process.env.NEXT_PUBLIC_WEBSITE_URL || 'http://localhost:3000';
+process.env.CLOUDMAILIN_USERNAME = process.env.CLOUDMAILIN_USERNAME || 'test-user';
+process.env.CLOUDMAILIN_API_KEY = process.env.CLOUDMAILIN_API_KEY || 'test-key';
